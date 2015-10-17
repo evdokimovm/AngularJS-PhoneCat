b5d9f8af08223e3ac7a87a22ca1f4c91a432e8c6
evdokimovm <tel89147925532@gmail.com>
2015-10-18 06:54:15 +1000

    Routing & Multiple Views

--- js/controllers.js
+++ js/controllers.js
@@ -1,29 +1,16 @@
-var phonecatApp = angular.module('phonecatApp', []);
+var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function ($scope, $http) {
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
+	function($scope, $http) {
+		$http.get('data/phones.json').success(function(data) {
+			$scope.phones = data;
+		});
 
-	$http.get('data/phones.json').success(function(data) {
-		$scope.phones = data;
-	});
-	
-	// $scope.phones = [
-	// 	{
-	// 		'name': 'Nexus S',
-	// 		'snippet': 'Fast just got faster with Nexus S.',
-	// 		'age': 1
-	// 	},
-	// 	{
-	// 		'name': 'Motorola XOOM with Wi-Fi',
-	// 		'snippet': 'The Next, Next Generation tablet.',
-	// 		'age': 2
-	// 	},
-	// 	{
-	// 		'name': 'MOTOROLA XOOM',
-	// 		'snippet': 'The Next, Next Generation tablet.',
-	// 		'age': 3
-	// 	}
-	// ];
+$scope.orderProp = 'age';
 
-	$scope.orderProp = 'age';
-	
 }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+	function($scope, $routeParams) {
+		$scope.phoneId = $routeParams.phoneId;
+	}]);
